a7d0d4f4ab96c7817d66b7386c18ba30f28c5886
tianyerr <t1244010727@163.com>
2023-02-10 17:26:36 +0800

    20230210001

---
+++ myweb/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='html'>
+      <div id='top_bar'>
+        dasdad
+      </div>
+      <div id='context'>
+        dsadasddadssadadas
+      </div>
+    </div>
+  )
+}
+
+export default App
